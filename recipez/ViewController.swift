f8f43da9af199ca5576aaf48f0e08a6dd138ced1
simpleCoding <simpleCoding@outlook.com>
2016-09-10 01:22:42 -0700

    Initial Commit

---
+++ recipez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recipez
+//
+//  Created by Daniel Ramirez on 9/10/16.
+//  Copyright © 2016 simpleCoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
